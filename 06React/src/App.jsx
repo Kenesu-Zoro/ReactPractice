edc429e32617402e61dd568d264ffa35e51b9703
Kenesu-Zoro <kennethdcosta29@gmail.com>
2025-03-13 23:02:12 +0530

    first commit

---
+++ 06React/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='font-bold bg-green-600 '>React Router</h1>
+    </>
+  )
+}
+
+export default App
