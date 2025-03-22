1d3ae5e04fd74a4814cc7a974e073b32496507dc
Kenesu-Zoro <kennethdcosta29@gmail.com>
2025-03-22 16:17:05 +0530

    "Todo Project using ContextApi and Local Storage"

--- 09TodoProjectReact/src/App.jsx
+++ 09TodoProjectReact/src/App.jsx
@@ -41,7 +41,7 @@ useEffect(() => {
     <TodoProvider value={{todos, addTodo, deleteTodo, updateTodo , toggleCompleted}}>
      <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-                    <h1 className="text-3xl font-bold text-center mb-8 mt-2">Manage your Todo's</h1>
+                    <h1 className="text-3xl font-bold text-center mb-8 mt-2">Manage your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
                         <TodoForm />
