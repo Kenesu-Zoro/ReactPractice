5509f460ee369439aaf817c47aa5bdc7a577019e
Kenesu-Zoro <kennethdcosta29@gmail.com>
2025-03-26 22:40:36 +0530

    "configured database and bucket(storage)"

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -16,7 +16,7 @@ export class AuthService{
         try{
             const userAccount = await this.account.create(ID.unique(), email , password , name );
             if (userAccount) {
-                //mehtod call
+                //method call
                 return this.login({email, password});
             } else {
                 return userAccount;
