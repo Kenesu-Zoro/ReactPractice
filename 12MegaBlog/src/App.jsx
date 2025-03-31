a4738dab5103d44f49feeef7243868e31f216972
Kenesu-Zoro <kennethdcosta29@gmail.com>
2025-03-31 23:12:04 +0530

    "container , Header and Button"

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,15 +1,43 @@
-
+import { useDispatch } from "react-redux"
+import { useState, useEffect } from 'react'
 import './App.css'
+import authService from "./appwrite/auth" 
+import {login , logout} from "./store/authSlice"
+import {Header, Footer} from "./components"
+
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+const [loading, setLoading] = useState(true)
+const dispatch = useDispatch() 
   
+useEffect(() => {
+  authService.getCurrentUser()
+  .then((userData) => {
+    if (userData) {
+      dispatch(login({userData}))
+    }else{
+      dispatch(logout())
+    }
+  })
+  .finally(() => setLoading(false))
+}, [])
 
-  return (
-    <>
-     <h1>Blog with Appwrite</h1>
-    </>
-  )
+
+return !loading ? (
+  <div className="min-h-screen flex justify-center bg-emerald-200">
+    <div className="w-full max-w-3xl mt-3 mx-auto text-center">
+      <Header />
+      <main className="my-8">
+         TODO:{/* <Outlet /> */}
+      </main>
+      <Footer />
+    </div>
+  </div>
+) : null;
+
+
+
 }
 
 export default App
